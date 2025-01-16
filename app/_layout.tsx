c3e419f40852a6afdf1a38b93acf5538d195e6a0
ateliti99 <aurelio.teliti@gmail.com>
2025-01-16 20:29:12 +0100

    Set the header bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,24 @@
 import { Stack } from "expo-router";
+import { useColorScheme } from "react-native";
+import { Colors } from "../constants/Colors";
 
 export default function RootLayout() {
-  return <Stack />;
+  const colorScheme = useColorScheme() || "light";
+  const theme = Colors[colorScheme];
+
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: true,
+        headerTitle: "EtiloMe",
+        headerStyle: {
+          backgroundColor: theme.systemBackground,
+        },
+        headerTintColor: theme.label,
+        headerTitleStyle: {
+          fontWeight: "bold",
+        },
+      }}
+    />
+  );
 }
