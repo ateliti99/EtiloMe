2d04dc8f3971969de97a1a3b9ae3823cf1802d51
ateliti99 <aurelio.teliti@gmail.com>
2025-01-17 00:03:56 +0100

    Refactor Index component to use SafeAreaView and add Summary and TestInformation sections

--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,40 @@
-import { Text, View } from "react-native";
 import React from "react";
+import { Text, View, StyleSheet } from "react-native";
 import { useColorScheme } from "react-native";
-import { Colors } from "../constants/Colors";
+import { Colors } from "@/constants/Colors";
+import Summary from "@/components/Summary";
+import SectionTitle from "@/components/SectionTitle";
+import TestInformation from "@/components/TestInformation";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   const colorScheme = useColorScheme() || "light";
   const theme = Colors[colorScheme];
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: theme.systemBackground,
-      }}
-    >
-      <Text style={{color: theme.label}}>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView style={[styles.main, {backgroundColor: theme.systemBackground}]}>
+      {/* Space */}
+      <View style={{ height: 20 }} />
+
+      {/* Summary */}
+      <SectionTitle title="Summary"/>
+      <Summary liters={50}/>
+
+      {/* Space */}
+      <View style={{ height: 20 }} />
+
+      {/* Alcohol Test */}
+      <SectionTitle title="Alcohol Test"/>
+      <TestInformation />
+
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
+});
